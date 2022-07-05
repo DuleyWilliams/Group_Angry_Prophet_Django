669cc0a0d2a37da6982601814bc0f12b6e63e199
Duley Williams <duleygrafix@gmail.com>
2022-07-05 18:25:07 -0500

    subscription

---
+++ rarerestapi/models/subscriptions.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from rarerestapi.models.rare_users import RareUser
+
+
+class Subscription(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+    """
+    follower_id = models.ForeignKey(RareUser, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(RareUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=55)
+    created_on = models.DateField()
+    ended_on = models.DateField()
