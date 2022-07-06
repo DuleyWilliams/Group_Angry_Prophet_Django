e5c1918bafe3b7539d6990b70d310bc80fef6d20
turner lynch <turnerjoylynch@gmail.com>
2022-07-05 18:40:33 -0700

    Migrations

--- rarerestapi/models/demotion_que.py
+++ rarerestapi/models/demotion_que.py
@@ -4,6 +4,6 @@ from django.db import models
 from rarerestapi.models.rare_users import RareUser
 
 class DemotionQue(models.Model):
-    action = models.CharField
-    admin_id = models.ForeignKey(RareUser, on_delete=models.CASCADE)
-    approver_one_id = models.ForeignKey(RareUser, on_delete=models.CASCADE)
+    action = models.CharField(max_length=100)
+    admin_id = models.ForeignKey(RareUser, on_delete=models.CASCADE, related_name="admin_id")
+    approver_one_id = models.ForeignKey(RareUser, on_delete=models.CASCADE, related_name="approver_one_id")
