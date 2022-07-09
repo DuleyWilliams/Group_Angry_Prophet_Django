533a48f78e7a6365411041c87ac45a0ae2c8ae04
ivanedrei <ivanramirez2014@gmail.com>
2022-07-09 09:34:54 -0500

    added fixtures, url routes, and created auth.py file for proper login/register

--- rarerest/urls.py
+++ rarerest/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from rarerestapi.views.auth import register_user, login_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register', register_user),
+    path('login', login_user)
 ]
